390ed7b0ec5ea1e7b711d7a1cb479d7be7b76914
saadahmde <saxdgraphics786@gmail.com>
2024-02-09 06:27:33 +0500

    Contact Management System

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 
-
 const connectDB = async () => {
     return mongoose
-    .connect('mongodb+srv://SaadAhmed:SaadAhmed@projectkit.l3tqm1r.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log(`connectino to the database established`))
-    .catch((err) =>  console.log(err))
+    .connect('mongodb+srv://SaadAhmed:1234567890@projectkit.l3tqm1r.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log(`Connection to the database established...`))
+    .catch((err) => console.log(err))
 }
 
 module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -3,22 +3,28 @@ const morgan = require('morgan');
 const connectDB = require('./config/db')
 
 
-
 const app = express();
 
 
-// middleware
-app.use(express.json());
-app.use(morgan('tiny'));
+// middlewares
+app.use(express.json()) // to send body data in json format
+app.use(morgan('tiny'))
+
 
 // routes
-app.get('/' , (req, res) => {
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
-// server configrations
+
+
+// server configration
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, async () => {
-    await connectDB();
-    console.log(`The server is listening to the http://localhost:${PORT}`)
+    try {
+        await connectDB();
+        console.log(`The server is listening to the http://localhost:${PORT}`)
+    } catch (error) {
+        return error
+    }
 })
